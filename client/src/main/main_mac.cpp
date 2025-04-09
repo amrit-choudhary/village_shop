b00ee016dd483f045b84193baa8bca5116b81af6
AmritChoudhary <albela.amrit@gmail.com>
2025-04-10 01:22:59 +0530

    Can see a simple window on mac.

--- client/src/main/main_mac.cpp
+++ client/src/main/main_mac.cpp
@@ -1,6 +1,12 @@
 #ifdef VG_MAC
 
+/**
+ * Main function for MacOS platform.
+ * Game loop remains the same, only windowing and rendering setup is different.
+ */
+
 #include <atomic>
+#include <cassert>
 #include <iostream>
 #include <thread>
 
@@ -15,13 +21,48 @@
 #include "src/misc/utils.h"
 #include "src/time/time_manager.h"
 
-int main(int argc, char **argv) {
+// Get MacOS specific includes.
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#include <AppKit/AppKit.hpp>
+#include <Metal/Metal.hpp>
+#include <MetalKit/MetalKit.hpp>
+
+using NS::StringEncoding::UTF8StringEncoding;
+
+class MyAppDelegate : public NS::ApplicationDelegate {
+   public:
+    ~MyAppDelegate();
+
+    NS::Menu* createMenuBar();
+
+    virtual void applicationWillFinishLaunching(NS::Notification* pNotification) override;
+    virtual void applicationDidFinishLaunching(NS::Notification* pNotification) override;
+    virtual bool applicationShouldTerminateAfterLastWindowClosed(NS::Application* pSender) override;
+
+   private:
+    NS::Window* _pWindow;
+};
+
+int main(int argc, char** argv) {
     ME::SetPaths(argv[0], argv[1]);
 
     // Read game params from file.
     INIMap iniMap = Load();
     int fps = std::atoi(iniMap["settings"]["fps"].c_str());
     int maxRunTime = std::atoi(iniMap["settings"]["maxRunTime"].c_str());
+
+    // MacOS application setup.
+    NS::AutoreleasePool* pAutoreleasePool = NS::AutoreleasePool::alloc()->init();
+
+    MyAppDelegate del;
+    NS::Application* pSharedApplication = NS::Application::sharedApplication();
+    pSharedApplication->setDelegate(&del);
+    pSharedApplication->run();
+
+    pAutoreleasePool->release();
 
     // Init global variables.
     ME::Time::TimeManager timeManager;
@@ -67,6 +108,79 @@ int main(int argc, char **argv) {
     game.End();
     renderer.End();
     connection.End();
+
+    return 0;
 }
 
-#endif
+MyAppDelegate::~MyAppDelegate() {
+    _pWindow->release();
+}
+
+NS::Menu* MyAppDelegate::createMenuBar() {
+    using NS::StringEncoding::UTF8StringEncoding;
+
+    NS::Menu* pMainMenu = NS::Menu::alloc()->init();
+    NS::MenuItem* pAppMenuItem = NS::MenuItem::alloc()->init();
+    NS::Menu* pAppMenu = NS::Menu::alloc()->init(NS::String::string("Village Game", UTF8StringEncoding));
+
+    NS::String* appName = NS::RunningApplication::currentApplication()->localizedName();
+    NS::String* quitItemName = NS::String::string("Quit ", UTF8StringEncoding)->stringByAppendingString(appName);
+    SEL quitCb = NS::MenuItem::registerActionCallback("appQuit", [](void*, SEL, const NS::Object* pSender) {
+        auto pApp = NS::Application::sharedApplication();
+        pApp->terminate(pSender);
+    });
+
+    NS::MenuItem* pAppQuitItem = pAppMenu->addItem(quitItemName, quitCb, NS::String::string("q", UTF8StringEncoding));
+    pAppQuitItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
+    pAppMenuItem->setSubmenu(pAppMenu);
+
+    NS::MenuItem* pWindowMenuItem = NS::MenuItem::alloc()->init();
+    NS::Menu* pWindowMenu = NS::Menu::alloc()->init(NS::String::string("Window", UTF8StringEncoding));
+
+    SEL closeWindowCb = NS::MenuItem::registerActionCallback("windowClose", [](void*, SEL, const NS::Object*) {
+        auto pApp = NS::Application::sharedApplication();
+        pApp->windows()->object<NS::Window>(0)->close();
+    });
+    NS::MenuItem* pCloseWindowItem = pWindowMenu->addItem(NS::String::string("Close Window", UTF8StringEncoding),
+                                                          closeWindowCb, NS::String::string("w", UTF8StringEncoding));
+    pCloseWindowItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
+
+    pWindowMenuItem->setSubmenu(pWindowMenu);
+
+    pMainMenu->addItem(pAppMenuItem);
+    pMainMenu->addItem(pWindowMenuItem);
+
+    pAppMenuItem->release();
+    pWindowMenuItem->release();
+    pAppMenu->release();
+    pWindowMenu->release();
+
+    return pMainMenu->autorelease();
+}
+
+void MyAppDelegate::applicationWillFinishLaunching(NS::Notification* pNotification) {
+    NS::Menu* pMenu = createMenuBar();
+    NS::Application* pApp = reinterpret_cast<NS::Application*>(pNotification->object());
+    pApp->setMainMenu(pMenu);
+    pApp->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
+}
+
+void MyAppDelegate::applicationDidFinishLaunching(NS::Notification* pNotification) {
+    CGRect frame = (CGRect){{100.0, 100.0}, {512.0, 512.0}};
+
+    _pWindow = NS::Window::alloc()->init(frame, NS::WindowStyleMaskClosable | NS::WindowStyleMaskTitled,
+                                         NS::BackingStoreBuffered, false);
+
+    _pWindow->setTitle(NS::String::string("Village Game", NS::StringEncoding::UTF8StringEncoding));
+
+    _pWindow->makeKeyAndOrderFront(nullptr);
+
+    NS::Application* pApp = reinterpret_cast<NS::Application*>(pNotification->object());
+    pApp->activateIgnoringOtherApps(true);
+}
+
+bool MyAppDelegate::applicationShouldTerminateAfterLastWindowClosed(NS::Application* pSender) {
+    return true;
+}
+
+#endif  // VG_MAC
