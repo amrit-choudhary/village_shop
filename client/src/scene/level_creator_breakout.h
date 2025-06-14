24b1c82ae2e71bb9837bfb2c7e4ce1143aadd5ea
AmritChoudhary <albela.amrit@gmail.com>
2025-06-14 23:37:51 +0530

    Started work on random level creator for Breakout.

---
+++ client/src/scene/level_creator_breakout.h
@@ -0,0 +1,17 @@
+/**
+ * Create random levels for the Breakout game.
+ * This function generates a level with a specified number of bricks and returns it.
+ * The level is represented as a 2D grid of bricks, where each brick has a type.
+ * The generated level can be used to initialize the game state when starting a new level.
+ */
+
+#pragma once
+
+namespace ME {
+
+class LevelCreatorBreakout {
+   public:
+    // static Level* CreateRandomLevel(uint16_t brickCount);
+};
+
+}  // namespace ME
