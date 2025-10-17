fc2b1f8c2dc435841af0e0315a75eff835776547
Amrit Choudhary <albela.amrit@gmail.com>
2025-10-18 04:30:23 +0530

    Temp commit.

--- client/src/rendering/directx/utils_directx.cpp
+++ client/src/rendering/directx/utils_directx.cpp
@@ -5,4 +5,13 @@
 #include "../shared/vertex.h"
 #include "src/math/vec2.h"
 
+ID3D12Resource* ME::UtilsDirectX::CreateDefaultBufferResource(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList,
+                                                              const void* initData, uint32_t byteSize,
+                                                              ID3D12Resource** uploadBuffer) {
+    ID3D12Resource* defaultBuffer = nullptr;
+    CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_DEFAULT);
+
+    return nullptr;
+}
+
 #endif  // VG_WIN
--- client/src/rendering/directx/utils_directx.h
+++ client/src/rendering/directx/utils_directx.h
@@ -6,9 +6,21 @@
  */
 
 #include <d3d12.h>
+#include <stdint.h>
+
+#include "d3dx12.h"
 
 namespace ME {
-namespace UtilsDirectX {}  // namespace UtilsDirectX
+namespace UtilsDirectX {
+
+/**
+ * Creates a default buffer resource in DirectX 12. Used for vertex and index buffers.
+ */
+static ID3D12Resource* CreateDefaultBufferResource(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList,
+                                                   const void* initData, uint32_t byteSize,
+                                                   ID3D12Resource** uploadBuffer);
+
+}  // namespace UtilsDirectX
 }  // namespace ME
 
 #endif  // VG_WIN
