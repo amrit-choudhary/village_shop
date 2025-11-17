7f5d5994e0c1673a2a1360a355bfce47ed8114a7
Amrit Choudhary <albela.amrit@gmail.com>
2025-11-17 16:51:14 +0530

    Added enemies animated spritesheet and required data.

---
+++ automation/common_commands.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# This is just a list of common commands used in various automation scripts.
+
+# Convert images to a single vertical image with transparent background
+# magick image1.png image2.png -background none -alpha set -append output.png
