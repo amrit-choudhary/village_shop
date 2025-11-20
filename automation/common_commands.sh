c7d433fad1e3c3014754d8deb179303a31e1ff4d
Amrit Choudhary <albela.amrit@gmail.com>
2025-11-20 05:43:36 +0530

    added gif commands.

--- automation/common_commands.sh
+++ automation/common_commands.sh
@@ -4,3 +4,8 @@
 
 # Convert images to a single vertical image with transparent background
 # magick image1.png image2.png -background none -alpha set -append output.png
+
+# Converting mp4 to gif
+# ffmpeg -y -i input.mp4 -vf "scale=iw:-1:flags=lanczos,palettegen=stats_mode=full" palette.png
+# ffmpeg -y -i input.mp4 -i palette.png -filter_complex "scale=iw:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=sierra2_4a" -loop 0 output.gif
+# rm palette.png
