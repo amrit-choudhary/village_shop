74fc838773d23c7885921fc25448e80c73bfd210
Amrit Choudhary <albela.amrit@gmail.com>
2025-12-28 23:38:05 +0530

    ui classes.

---
+++ client/src/ui/geometry.cpp
@@ -0,0 +1,29 @@
+#include "geometry.h"
+
+ME::Geometry::Geometry() {}
+
+ME::Geometry::~Geometry() {}
+
+ME::Vec2 ME::Geometry::GetPosition() const {
+    return position;
+}
+
+void ME::Geometry::SetPosition(const Vec2& pos) {
+    position = pos;
+}
+
+ME::Vec2 ME::Geometry::GetAbsolutePosition() const {
+    return absolutePosition;
+}
+
+void ME::Geometry::SetAbsolutePosition(const Vec2& absPos) {
+    absolutePosition = absPos;
+}
+
+ME::Vec2 ME::Geometry::GetSize() const {
+    return size;
+}
+
+void ME::Geometry::SetSize(const Vec2& sz) {
+    size = sz;
+}
---
+++ client/src/ui/geometry.h
@@ -0,0 +1,31 @@
+#pragma once
+
+/**
+ * Geometry definitions for UI elements.
+ */
+
+#include "src/math/vec2.h"
+
+namespace ME {
+
+class Geometry {
+   public:
+    Geometry();
+    ~Geometry();
+
+    // Getters and setters for position and size
+    Vec2 GetPosition() const;
+    void SetPosition(const Vec2& pos);
+    Vec2 GetAbsolutePosition() const;
+    void SetAbsolutePosition(const Vec2& absPos);
+    Vec2 GetSize() const;
+    void SetSize(const Vec2& sz);
+
+   private:
+    Vec2 position = Vec2();          // local position
+    Vec2 absolutePosition = Vec2();  // absolute position in the UI
+    Vec2 size = Vec2();              // local size
+    Vec2 absoluteSize = Vec2();      // absolute size in the UI
+};
+
+}  // namespace ME
